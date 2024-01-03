516e76099f1a7c5ac4c9c15cc73ec35a911fbe79
Samaha Musthafa <samahasama23@gmail.com>
2024-01-03 11:17:15 +0530

    netflix-last commit

--- src/App.js
+++ src/App.js
@@ -3,13 +3,19 @@ import NavBar from './Components/NavBar/NavBar';
 import Banner from './Components/Banner/Banner';
 import './App.css'
 import RowPost from './Components/RowPost/RowPost';
+import { action, comedy,horror ,documentaries, originals, romance } from './urls';
 
 function App() {
   return (
     <div className="app">
       <NavBar/>
       <Banner/>
-      <RowPost/>
+      <RowPost url={originals} title='Netflix Originals'/>
+      <RowPost url={action} title='Action' isSmall/>
+      <RowPost url={comedy} title='Comedy' isSmall/>
+      <RowPost url={horror} title='Horror' isSmall/>
+      <RowPost url={romance} title='Romance' isSmall/>
+      <RowPost url={documentaries} title='Documentaries' isSmall/>
     </div>
   );
 }
---
+++ src/constants/constants.js
@@ -0,0 +1,3 @@
+export const baseUrl = 'https://api.themoviedb.org/3';
+export const API_KEY = "9dae42067d748d760b2bbef8ccfe5f5f";
+export const imageUrl =  'https://image.tmdb.org/t/p/original';
